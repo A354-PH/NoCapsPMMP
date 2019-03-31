b7a506e4c6c7f9f6cf1897f529d50aaee69f91f7
Jan Leigh <40911019+A354-PH@users.noreply.github.com>
2019-03-31 10:10:59 +0800

    Plugin Done.

---
+++ src/NoCaps/Main.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace NoCaps;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+
+class Main extends PluginBase implements Listener {
+  
+  public function onEnable() {
+    $this->getServer()->getPluginManager->registerEvents($this, $this);
+    $this->getLogger()->info("NoCaps has been enabled! A plugin ported from NukkitX");
+  }
+  
+  public function onChat(PlayerChatEvent $event) {
+    $player = $event->getPlayer();
+    if(!$player->hasPermission("nocaps.avoidchecking")) {
+      $event->setMessage($event->getMessage()->toLowerCase());
+    }
+  }
+}

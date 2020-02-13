b055f47234fc4391c8030d21d0cbe11fb94d4e4b
Kizu <jajaskie0403@gmail.com>
2020-02-13 19:33:38 +0800

    Plugin is now multi-byte safe

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # NoCaps
+
 Ported from NukkitX. Makes uppercase letters to lowercase letters
 
-## Permissions
-- nocaps.avoidchecking = Ignores the player
+-----
 
-## Bugs/Contact
+### Installation
 
-DM me on Twitter(@TheRealKizu) or on Discord(TheRealKizu#3267)!
+- Download the latest or stable build. Builds can be found [here](https://poggit.pmmp.io/p/NoCapsPMMP/)
+- Place it in your `plugins` folder.
+- Restart your server.
+
+### Permissions
+
+`nocaps.avoidchecking` = Ignores the player with a uppercase message
+
+### Support
+  
+Suggestions? Bugs? Need help? Join my discord server down below!
+  
+<a href="https://discord.gg/SdjhdXp"><img src="https://discordapp.com/api/guilds/638310885118574602/embed.png" alt="Discord Server"/></a>
---
+++ src/TheRealKizu/NoCaps/NoCaps.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ *  Copyright (C) 2019-2020 TheRealKizu
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace TheRealKizu\NoCaps;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\plugin\PluginBase;
+
+class NoCaps extends PluginBase implements Listener
+{
+
+    private static $instance;
+
+    public static function getInstance(): self
+    {
+        return self::$instance;
+    }
+
+    public function onLoad()
+    {
+        self::$instance = $this;
+    }
+
+    public function onEnable()
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("NoCapsPMMP is now enabled! Made by (@TheRealKizu)");
+    }
+
+    /**
+     * @param PlayerChatEvent $event
+     */
+    public function onChat(PlayerChatEvent $event)
+    {
+        $player = $event->getPlayer();
+        if (!$player->hasPermission("nocaps.avoidchecking")) {
+            $msg = $event->getMessage();
+            $event->setMessage(mb_strtolower($msg, "UTF-8"));
+        }
+    }
+}
